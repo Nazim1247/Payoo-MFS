6ae679f62bfa81784f0d527a19b514bf0ff86236
nazim uddin <mdnajim1247@gmail.com>
2024-09-19 09:43:15 +0600

    add mony done

--- js/home.js
+++ js/home.js
@@ -2,8 +2,27 @@
 // step 1
 document.getElementById('btn-add-mony').addEventListener('click', function(event){
     event.preventDefault();
+    // step 2
     const addMonyInput = document.getElementById('input-add-mony').value;
     console.log(addMonyInput);
-    const pinNumber = document.getElementById('input-pin-number').value;
-    console.log(pinNumber);
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(typeof pinNumberInput);
+    // step 3
+    if(pinNumberInput === '1234'){
+        console.log('adding mony to your account');
+        // step 4
+    const balance = document.getElementById('account-balance').innerText;
+    console.log(typeof balance);
+    // step 5
+    const addMonyNumber = parseFloat(addMonyInput);
+    const balanceNumber = parseFloat(balance);
+    const newBalance = addMonyNumber + balanceNumber;
+    console.log(newBalance);
+    // step 6
+    document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add mony! please try again')
+    }
+    
 })
